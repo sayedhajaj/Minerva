bfee9ff3f75e53c413e3d282b4d498b71a135574
Sayed Hajaj <sayedhajaj@gmail.com>
2022-08-05 22:08:10 +0100

    Add inheritance test

---
+++ test/InheritanceTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+
+class InheritanceTest {
+
+    @Test
+    internal fun testInheritance() {
+        val source = HelloWorldTest::class.java.getResource("inheritance.minerva").readText()
+
+        val syntaxTree = Minerva.getSyntaxTree(source)
+
+        val output = Minerva.interpret(syntaxTree).toTypedArray()
+        assertContentEquals(arrayOf("Fry until golden brown.", "Pipe full of custard and coat with chocolate."), output, "")
+    }
+}
