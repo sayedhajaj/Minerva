7b46d76c2b2bcf06ee161f0117eff44e6591ce04
Sayed Hajaj <sayedhajaj@gmail.com>
2022-08-06 13:19:51 +0100

    Add classes test

---
+++ test/ClassesTest.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertContentEquals
+
+class ClassesTest {
+    @Test
+    internal fun testClasses() {
+        val source = HelloWorldTest::class.java.getResource("classes.minerva").readText()
+
+        val syntaxTree = Minerva.getSyntaxTree(source)
+
+        val output = Minerva.interpret(syntaxTree).toTypedArray()
+        assertContentEquals(arrayOf("12", "14", "6", "20", "18"), output, "")
+    }
+}
