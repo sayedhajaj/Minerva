85d2deb9488d6757279636402bda24c038201222
Sayed Hajaj <sayedhajaj@gmail.com>
2022-08-12 16:35:15 +0100

    Test closes over free variables

--- test/functions/ClosureTest.kt
+++ test/functions/ClosureTest.kt
@@ -7,12 +7,22 @@ import kotlin.test.assertContentEquals
 
 class ClosureTest {
     @Test
-    internal fun testClosures() {
-        val source = HelloWorldTest::class.java.getResource("functions/closures.minerva").readText()
+    internal fun testCounter() {
+        val source = HelloWorldTest::class.java.getResource("functions/closures/counter.minerva").readText()
 
         val syntaxTree = Minerva.getSyntaxTree(source)
 
         val output = Minerva.interpret(syntaxTree).toTypedArray()
         assertContentEquals(arrayOf("1", "2"), output, "")
+    }
+
+    @Test
+    internal fun testClosesOverFreeVariables() {
+        val source = HelloWorldTest::class.java.getResource("functions/closures/closes_over_free_variables.minerva").readText()
+
+        val syntaxTree = Minerva.getSyntaxTree(source)
+
+        val output = Minerva.interpret(syntaxTree).toTypedArray()
+        assertContentEquals(arrayOf("global", "global"), output, "")
     }
 }
