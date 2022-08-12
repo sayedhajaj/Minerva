187e9ec991762135650f24e2ff93cbbb0f0c9780
Sayed Hajaj <sayedhajaj@gmail.com>
2022-08-12 17:36:17 +0100

    Test dangling else

--- test/controlFlow/IfTest.kt
+++ test/controlFlow/IfTest.kt
@@ -9,11 +9,22 @@ class IfTest {
 
     @Test
     internal fun testIf() {
-        val source = HelloWorldTest::class.java.getResource("control_flow/if.minerva").readText()
+        val source = HelloWorldTest::class.java.getResource("control_flow/if/if.minerva").readText()
 
         val syntaxTree = Minerva.getSyntaxTree(source)
 
         val output = Minerva.interpret(syntaxTree).toTypedArray()
         assertContentEquals(arrayOf("You are an adult", "adult"), output, "")
     }
+
+    @Test
+    internal fun testDanglingElse() {
+        val source = HelloWorldTest::class.java.getResource("control_flow/if/dangling_else.minerva").readText()
+
+        val syntaxTree = Minerva.getSyntaxTree(source)
+
+        val output = Minerva.interpret(syntaxTree).toTypedArray()
+        assertContentEquals(arrayOf("good"), output, "")
+    }
+
 }
